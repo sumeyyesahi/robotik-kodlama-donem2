c13fd281e1abbbe05e1e142051cdd2272161a901
sumeyyesahi <sumeyyehnk@gmail.com>
2025-04-22 14:56:30 +0300

    Add files via upload

---
+++ 250422/Hello_World/Hello_World.ino
@@ -0,0 +1,59 @@
+/*
+    LCD_I2C - Arduino library to control a 16x2 LCD via an I2C adapter based on PCF8574
+
+    Copyright(C) 2020 Blackhack <davidaristi.0504@gmail.com>
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#include <LCD_I2C.h>
+
+LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
+
+#include <Wire.h>
+
+
+const int trig=13;
+const int echo=12;
+
+void setup()
+{
+    pinMode(trig,OUTPUT);
+    pinMode(echo,INPUT);
+    lcd.begin(); 
+    lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print("Mesafe");
+}
+
+void loop()
+{
+    long sure;
+    float mesafe;
+    int boy=190;
+   
+    digitalWrite(trig,0);
+    delayMicroseconds(2);
+    digitalWrite(trig,1);
+    delayMicroseconds(10);
+    digitalWrite(trig,0);
+
+    sure=pulseIn(echo,1);
+    mesafe=(sure*0.0343)/2;
+    mesafe=mesafe-boy;
+    lcd.setCursor(0,1);
+    lcd.print(mesafe);
+    lcd.print("cm     ");
+    delay(500);
+}
